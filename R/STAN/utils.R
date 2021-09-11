7705aaf911befac1a8905774996ca0c5aa0a3167
Marcos Prates <marcosop@gmail.com>
2021-09-10 21:55:03 -0300

    update results_09_10_2021_21:55:03

--- R/STAN/utils.R
+++ R/STAN/utils.R
@@ -2,6 +2,35 @@ classify.flag = function(obs, adj){
   
   colnames(obs) = c("date", "y")
   
+  dados.aux = suppressMessages(full_join(obs, adj) %>%
+                                 mutate(y_s = rollmean(y, k = 20, fill = NA) %>% rollmean(k = 10, fill = NA),   # suavizando os dados
+                                        dif_s = c(NA, y_s %>% diff()) %>% rollmean(k = 50, fill = NA),          # diferença suavizada 
+                                        id = c(NA, dif_s %>% sign() %>% diff()) ) )                               # identificador de mudança de comportamento
+  
+  maxx = max(dados.aux$y_s, na.rm = TRUE)
+  
+  aux = dados.aux %>% filter(id == 2 |id == -2) %>% arrange(desc(date))
+  
+  if(aux$id[1] == -2){ # se o último ponto identificado é um pico, a amplitude é calculada com base na última observação do y suavizado
+    aux = rbind(tail(dados.aux %>% filter(!is.na(y_s)), 1) %>% mutate(id = 2), aux) 
+  }
+  
+  est = aux %>% mutate(amp_y = c(NA, diff(y_s)), 
+                       amp_mu = c(NA, diff(mu)),
+                       id_df = c(NA, diff(id)),
+                       est = (amp_y - amp_mu)^2/maxx^2) %>%
+    filter(id_df == -4) # amplitudes corretas
+  
+  est1 = (est %>% summarise(crit = sum(est)))$crit
+  
+  est1
+  
+}
+
+classify.flag.old = function(obs, adj){
+  
+  colnames(obs) = c("date", "y")
+  
   dados = full_join(obs, adj) %>% mutate(y_s = rollmean(y, k = 20, fill = NA) %>% rollmean(k = 10, fill = NA),   # suavizando os dados
                                          dif_s = c(NA, y_s %>% diff()) %>% rollmean(k = 50, fill = NA),          # diferença suavizada 
                                          id = c(NA, dif_s %>% sign() %>% diff()) )                               # identificador de mudança de comportamento
@@ -19,6 +48,6 @@ classify.flag = function(obs, adj){
            filter(id_df == -4) %>% # amplitudes corretas
            summarise(crit = sum(est)))$crit
   
-  ifelse(est <= 0.2, 0, ifelse(est <= 0.5, 1, 2))
-  
+  #ifelse(est <= 0.2, 0, ifelse(est <= 0.5, 1, 2))
+  est
 }
